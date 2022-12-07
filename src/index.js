d8960a07d9f7f6bc9a7b603e7205bd8e2993b4b8
minhtruc <truc0979454023@gmail.com>
2022-12-08 03:14:21 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:5000";
 
 ReactDOM.render(<App />, document.getElementById("root"));
